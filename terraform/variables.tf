66c714b11dc7ace9b36be16b5eff3b4a830c10fb
Momrad <adamgames2003@gmail.com>
2025-06-06 15:08:58 +0200

    added vpc

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,11 @@ variable "operation-iam-role" {
 variable "aws_region" {
   default = "eu-central-1"
 }
+
+variable "vpc_name" {
+  default = "main-vpc"
+}
+
+variable "vpc_cidr_block" {
+  default = "192.168.0.0/16"
+}
