a3f51b6c7b1f14124c0d608a200545272ec3f4b8
Momrad <114695542+Momrad@users.noreply.github.com>
2025-06-09 17:24:05 +0200

    added clodufront module

---
+++ terraform/modules/cloudfront_distributor/main.tf
@@ -0,0 +1,36 @@
+resource "aws_cloudfront_distribution" "html_distribution" {
+  origin {
+    domain_name              = var.bucket_regional_domain_name
+    origin_id                = var.s3_origin_id
+
+    origin_access_control_id = var.origin_access_control_id
+    # s3_origin_config {
+    #   origin_access_identity = var.origin_access_identity
+    # }
+  }
+  enabled             = var.distribution_enabled
+  is_ipv6_enabled     = true
+  default_root_object = var.default_root_object
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  default_cache_behavior {
+    target_origin_id       = var.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    compress = true
+  }
+}
---
+++ terraform/modules/cloudfront_distributor/variables.tf
@@ -0,0 +1,25 @@
+variable "bucket_regional_domain_name" {
+  type = string
+}
+
+variable "origin_access_control_id" {
+  type = string
+}
+
+variable "s3_origin_id" {
+  type = string
+}
+
+variable "distribution_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "default_root_object" {
+  type    = string
+  default = "index.html"
+}
+
+# variable "origin_access_identity" {
+#   type = string
+# }
