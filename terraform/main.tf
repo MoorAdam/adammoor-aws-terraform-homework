62152938388616373ae1f66bef890500efd05931
Momrad <114695542+Momrad@users.noreply.github.com>
2025-06-09 11:25:32 +0200

    added subnets to root file

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,21 @@ module "vpc" {
   vpc_name = var.vpc_name
   vpc_cidr_block = var.vpc_cidr_block
 }
+
+module "public_subnet" {
+  source = "./modules/subnet"
+
+  vpc_id = module.vpc.vpc_id
+  subnet_name = var.public_subnet_name
+  subnet_cidr = var.public_subnet_cidr_block
+  subnet_availability_zone = true
+}
+
+module "private_subnet" {
+  source = "./modules/subnet"
+
+  vpc_id = module.vpc.vpc_id
+  subnet_name = var.private_subnet_name
+  subnet_cidr = var.private_subnet_cidr_block
+  subnet_availability_zone = false
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,20 @@ variable "aws_region" {
 variable "vpc_name" {
   default = "main-vpc"
 }
-
 variable "vpc_cidr_block" {
   default = "192.168.0.0/16"
+}
+
+variable "public_subnet_name" {
+  default = "public-subnet"
+}
+variable "public_subnet_cidr_block" {
+  default = "192.168.1.0/24"
+}
+
+variable "private_subnet_name" {
+  default = "private-subnet"
+}
+variable "private_subnet_cidr_block" {
+  default = "192.168.2.0/24"
 }
